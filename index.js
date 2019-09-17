76303d78d21ce68d3379ae0b8cf6886354b4dee7
Nataliya R <n.rudenko@gmail.com>
2019-09-17 20:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ class Driver {
     this.name = name
     store.drivers.push(this)
   }
-  
+  trips() {
+    return store.trips.filter(trip => trip.driverId === this.id)
+  }
 }
 
 let passengerId = 1
